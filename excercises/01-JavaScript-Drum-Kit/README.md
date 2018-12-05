936e2b4dcbe8efc435a0554f5cc94570db3a0741
annie tran <nntrn@me.com>
2018-12-05 00:27:41 -0600

    01: click and keydown event listener

--- excercises/01-JavaScript-Drum-Kit/README.md
+++ excercises/01-JavaScript-Drum-Kit/README.md
@@ -7,31 +7,18 @@
 
 ### html
 
-added `<meta name="viewport" content="width=device-width, initial-scale=1">` to the `<head>`
+added viewport meta tag for mobile browsers: 
+`<meta name="viewport" content="width=device-width, initial-scale=1">` 
 
 ### CSS:
 
+added after `style.css`
+
 ```css
-body {
-    min-height: 100vh;
-    overflow: hidden;
-}
-
-.keys {
-    flex-wrap: wrap;
-    margin-top: 20vh;
-    min-height: unset;
-}
-
-.key {
-    width: 7rem;
-    height: 7rem;
-    padding: unset;
-}
-
-.key:hover {
-    background: rgba(0,0,0,.8);
-}
+body {min-height:100vh; overflow:hidden; }
+.keys {flex-wrap:wrap; margin-top:20vh; min-height:unset; user-select: none;}
+.key {width:7rem; height:7rem; padding:unset; }
+.key:hover, .playing {background:rgba(0,0,0,.8); }  
 ```
 
 
@@ -44,7 +31,20 @@ Do something to `keys` element when a CSS transition has ended:
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 ```
 [source](https://www.w3schools.com/jsref/event_transitionend.asp)
+[further reading](https://jonsuh.com/blog/detect-the-end-of-css-animations-and-transitions-with-javascript/)
 
-----
-* [template literal](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)
+### template literal
+
+```js
+function playKeySound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
+  }
+```
+[source](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals)
 
--- excercises/01-JavaScript-Drum-Kit/script.js
+++ excercises/01-JavaScript-Drum-Kit/script.js
@@ -11,13 +11,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
   audio.forEach(function (element, i) {
     sound[i] = audio[i].getAttribute("src");
     key[i] = audio[i].getAttribute("data-key");
-    //console.log(sound[i] + " " + key[i]);
   });
 
-  var playSound = function () {
+  var playKeySound = function(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add("playing");
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  var playClickSound = function () {
     var dataKey = this.getAttribute("data-key");
     getSound(dataKey).play();
-    this.classList.add('playing');
+    this.classList.add("playing");
   }
 
   var getSound = function (num) {
@@ -26,27 +35,13 @@ document.addEventListener("DOMContentLoaded", function (event) {
     return audioToPlay;
   }
 
-  keys.forEach(cell => cell.addEventListener("click", playSound));
-
-
-   /* KEY DOWN
-   ************************* */
-  function removeTransition(e) {
-    if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
+  var removeTransition = function(e){
+    if (e.propertyName !== "transform") return;
+    e.target.classList.remove("playing");
   }
 
-  function playKeySound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if (!audio) return;
-
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
-
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playKeySound);
+  keys.forEach(cell => cell.addEventListener("click", playClickSound));
+  keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+  window.addEventListener("keydown", playKeySound);
 
 });
